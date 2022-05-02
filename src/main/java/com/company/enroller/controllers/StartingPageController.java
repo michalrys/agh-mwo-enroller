53e92f635273bee49b94ab643141be55c2f89de7
michalrys <michalrys@gmail.com>
2022-05-02 23:25:13 +0200

    plain html page for test only

---
+++ src/main/java/com/company/enroller/controllers/StartingPageController.java
@@ -0,0 +1,13 @@
+package com.company.enroller.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class StartingPageController {
+
+    @RequestMapping(value = "")
+    public String mainPage() {
+        return "index";
+    }
+}
