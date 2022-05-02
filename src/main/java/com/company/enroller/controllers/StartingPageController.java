8d4b005a89246f04bc20dbef80b697f08067d69b
michalrys <michalrys@gmail.com>
2022-05-02 23:41:33 +0200

    fix: simple main page info

--- src/main/java/com/company/enroller/controllers/StartingPageController.java
+++ src/main/java/com/company/enroller/controllers/StartingPageController.java
@@ -10,6 +10,27 @@ public class StartingPageController {
     @RequestMapping(value = "/")
     @ResponseBody
     public String mainPage() {
+        String simpleInfo =
+                "<!DOCTYPE html>\n" +
+                        "<html lang=\"en\">\n" +
+                        "<head>\n" +
+                        "    <meta charset=\"UTF-8\">\n" +
+                        "    <title>Zadanie</title>\n" +
+                        "</head>\n" +
+                        "<body>\n" +
+                        "\n" +
+                        "Michał R - zadanie:<br>\n" +
+                        "Na ocenę 4.0:<br>\n" +
+                        "\n" +
+                        "    <br>Pobieranie listy wszystkich spotkań\n" +
+                        "    <br>Pobieranie listy pojedyncznego spotkania\n" +
+                        "    <br>Dodawanie spotkań\n" +
+                        "    <br>Dodawanie uczestnika do spotkania\n" +
+                        "    <br>Pobieranie uczestników spotkania" +
+                        "<br><br><a href=\"https://github.com/michalrys/agh-mwo-enroller\" target=new>https://github.com/michalrys/agh-mwo-enroller</a>" +
+                        "</body>\n" +
+                        "</html>";
+
         return "index";
     }
 }
